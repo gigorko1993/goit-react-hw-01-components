8f281539c0e2740116d753bf6c5f6e17010559ad
gigorko1993 <gigorko1993@gmail.com>
2021-07-10 23:42:44 +0300

    refactoring 1 task

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ import transactions from "../src/Data/transactions.json";
 export default function App() {
   return (
     <div>
-      <Profile items={user} />
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics stats={stat} />
       <FriendList items={friends} />
       <TransactionHistory items={transactions} />
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,24 +1,41 @@
-import ProfileItem from "../Components/ProfileItem";
+import PropTypes from "prop-types";
 
-const Profile = ({ items }) => {
+const Profile = ({ avatar, name, tag, location, followers, views, likes }) => {
   return (
-    <ul>
-      {items.map((item) => {
-        return (
-          <li>
-            <ProfileItem
-              name={item.name}
-              tag={item.tag}
-              location={item.location}
-              avatar={item.avatar}
-              followers={item.stats.followers}
-              views={item.stats.views}
-              likes={item.stats.likes}
-            />
-          </li>
-        );
-      })}
-    </ul>
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt="Аватар пользователя" class="avatar" />
+        <p class="name">{name}</p>
+        {tag ? <p class="tag">@{tag}</p> : <p class="tag">no tag</p>}
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
   );
 };
+
+Profile.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  followers: PropTypes.number,
+  likes: PropTypes.number,
+  views: PropTypes.number,
+};
+
 export default Profile;
