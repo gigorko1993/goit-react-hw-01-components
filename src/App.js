c83a1f8b4471608a234a8e70bbf8f3468be96e33
gigorko1993 <gigorko1993@gmail.com>
2021-07-10 18:03:49 +0300

    finished 1 task

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import User from "../src/Components/User";
+import UserList from "../src/Components/UserList";
 import user from "../src/user.json";
 
 export default function App() {
   return (
     <div>
-      <User
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        followers={user.stats.followers}
-        views={user.stats.views}
-        likes={user.stats.likes}
-      />
+      <UserList items={user} />
     </div>
   );
 }
