f5f57b7406909a776618a87672179366855013fe
gigorko1993 <gigorko1993@gmail.com>
2021-07-10 18:15:51 +0300

    finished tsk 02

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -3,19 +3,21 @@ import User from "../Components/User";
 const UserList = ({ items }) => {
   return (
     <ul>
-      {items.map((item) => (
-        <li>
-          <User
-            name={item.name}
-            tag={item.tag}
-            location={item.location}
-            avatar={item.avatar}
-            followers={item.stats.followers}
-            views={item.stats.views}
-            likes={item.stats.likes}
-          />
-        </li>
-      ))}
+      {items.map((item) => {
+        return (
+          <li>
+            <User
+              name={item.name}
+              tag={item.tag}
+              location={item.location}
+              avatar={item.avatar}
+              followers={item.stats.followers}
+              views={item.stats.views}
+              likes={item.stats.likes}
+            />
+          </li>
+        );
+      })}
     </ul>
   );
 };
